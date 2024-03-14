8263f49f168bd58e779fed8421693eb93e827ea2
Izzy <tellmlaye@gmail.com>
2024-03-15 02:48:31 +0800

    Discord Bot

---
+++ DiscordBot/bot.py
@@ -0,0 +1,25 @@
+import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('MTIxNzg4MTE3NDYwOTg5MTMyOA.G_Lg3c.Cj5s-RYdg-Iu1_He1mqg9WnI1ZD-iSbvj__pEU')  # Replace with your own token.
